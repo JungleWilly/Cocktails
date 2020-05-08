13c5192e156bc1adf004fc26275372a53e658571
junglewilly <guillaumec.dev@gmail.com>
2020-05-08 14:57:23 +0200

    implement create ingredients feature

--- cocktailProjet/src/app/cocktail-container/cocktail-edit/cocktail-edit.component.ts
+++ cocktailProjet/src/app/cocktail-container/cocktail-edit/cocktail-edit.component.ts
@@ -3,9 +3,11 @@ import {
   FormControl,
   FormGroup,
   FormBuilder,
-  Validator,
   Validators,
+  FormArray,
 } from '@angular/forms';
+
+import { CocktailService } from '../../shared/services/cocktail.service';
 @Component({
   selector: 'app-cocktail-edit',
   templateUrl: './cocktail-edit.component.html',
@@ -13,17 +15,35 @@ import {
 })
 export class CocktailEditComponent implements OnInit {
   public cocktail: FormGroup;
-  constructor(private fb: FormBuilder) {}
+
+  constructor(
+    private fb: FormBuilder,
+    private cocktailService: CocktailService
+  ) {}
 
   ngOnInit(): void {
     this.cocktail = this.fb.group({
       name: ['', Validators.required],
       img: ['', Validators.required],
       desc: [''],
+      ingredients: this.fb.array([]),
     });
   }
 
+  get ingredients() {
+    return <FormArray>this.cocktail.get('ingredients');
+  }
+
+  addIngredient(): void {
+    (<FormArray>this.cocktail.get('ingredients')).push(
+      this.fb.group({
+        name: [''],
+        quantity: [''],
+      })
+    );
+  }
+
   createCocktail() {
-    console.log(this.cocktail);
+    this.cocktailService.addCocktail(this.cocktail.value);
   }
 }
